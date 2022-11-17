d3e48544cbe63af210ce9799b40d941a5fd6817d
Yllka Shala <yllkaaa.sha@gmail.com>
2022-11-17 14:46:47 +0100

    movies, lists, bookmarks

---
+++ app/models/bookmark.rb
@@ -0,0 +1,9 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :comment, length: { minimum: 6 }
+  validates :movie, uniqueness: {
+    scope: [ :movie_id, :list_id ]
+  }
+end
